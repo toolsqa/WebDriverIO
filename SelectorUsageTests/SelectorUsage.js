3af8166ce5cb4a42bbb6d28e710c37eafffb7e23
Virender Singh <virenv@outlook.com>
2016-11-01 09:36:14 +0530

    Adding some text locator stratedgy to tests

--- SelectorUsageTests/SelectorUsage.js
+++ SelectorUsageTests/SelectorUsage.js
@@ -14,14 +14,14 @@
   WebDriver.getElement(By.id())
 */
 
-describe('Selecting element by ID on http://toolsqa.com//automation-practice-switch-windows', function (){
-	it('Tests that we can select an element by ID by using # as the keyword for id', function () {
-	   
-		browser.url("http://toolsqa.com/automation-practice-switch-windows/");
-		//See how # can be used to find an element by ID. Text following # represents and ID
-		browser.click("#button1");
+	describe('Selecting element by ID on http://toolsqa.com//automation-practice-switch-windows', function (){
+		it('Tests that we can select an element by ID by using # as the keyword for id', function () {
+		   
+			browser.url("http://toolsqa.com/automation-practice-switch-windows/");
+			//See how # can be used to find an element by ID. Text following # represents and ID
+			browser.click("#button1");
+		});
 	});
-});
 
 /*
   In this test we will see how we can select an element by CSS path.
@@ -30,13 +30,13 @@ describe('Selecting element by ID on http://toolsqa.com//automation-practice-swi
   Note: WebDriverIO has default search stratedgy set to CSS Path
 */
 
-describe('Selecting element by CSS path on http://toolsqa.com//automation-practice-switch-windows', function (){
-	it('Tests that we can select an element by CSS by simply specifying the CSS path', function () {
-	   
-		browser.url("http://toolsqa.com/automation-practice-switch-windows/");
-		browser.click("#content > p:nth-child(5) > button");
+	describe('Selecting element by CSS path on http://toolsqa.com//automation-practice-switch-windows', function (){
+		it('Tests that we can select an element by CSS by simply specifying the CSS path', function () {
+		   
+			browser.url("http://toolsqa.com/automation-practice-switch-windows/");
+			browser.click("#content > p:nth-child(5) > button");
+		});
 	});
-});
 
 
 /*
@@ -47,13 +47,13 @@ describe('Selecting element by CSS path on http://toolsqa.com//automation-practi
   Note: Pay attention to the usage of '=' to specify that the text following it is link text
 */
 
-describe('Selecting element by Link text on http://toolsqa.com/automation-practice-form/', function (){
-	it('Tests that we can select an element by specifying the link text', function () {
-		browser.url("http://toolsqa.com/automation-practice-form/");
-		//= specifies that the text following is the complete visible text of a link element
-		browser.click("=Link Test");
+	describe('Selecting element by Link text on http://toolsqa.com/automation-practice-form/', function (){
+		it('Tests that we can select an element by specifying the link text', function () {
+			browser.url("http://toolsqa.com/automation-practice-form/");
+			//= specifies that the text following is the complete visible text of a link element
+			browser.click("=Link Test");
+		});
 	});
-});
 
 /*
   In this test we will see how we can select an element by Partial Link text.
@@ -63,18 +63,37 @@ describe('Selecting element by Link text on http://toolsqa.com/automation-practi
   Note: Pay attention to the usage of '*=' to specify that the text following it is partial link text
 */
 
-describe('Selecting element by partial Link text on http://toolsqa.com/automation-practice-form/', function (){
-	it('Tests that we can select an element by specifying the partial link text', function* (done) {
-		browser.url("http://toolsqa.com/automation-practice-form/");
-		//*= specifies that the text following is the partial visible text of a link element
-	    browser.click("*=Partial Link").then(function() {
-			console.log("The page title is " + browser.getTitle());
-		}).call(done);
+	describe('Selecting element by partial Link text on http://toolsqa.com/automation-practice-form/', function (){
+		it('Tests that we can select an element by specifying the partial link text', function* (done) {
+			browser.url("http://toolsqa.com/automation-practice-form/");
+			//*= specifies that the text following is the partial visible text of a link element
+			browser.click("*=Partial Link").then(function() {
+				console.log("The page title is " + browser.getTitle());
+			}).call(done);
+		});
 	});
-});
 
+/*
+  This is locator technique is unique to WebDriverIO and is not available in WebDriver Java bindings.
+  You can identify elements using some text of the element. Just make sure that the text is unique to
+  that element. Something like this
+*/
 
-
+	describe("Selecting element by the text available on the element", function () {
+		it("Tests that we can select an element by using text value associated with the element", function (){
+			browser.url("http://toolsqa.com/automation-practice-switch-windows/");
+			var textOfElement = browser.getText("a=Find me I have nothing in me!!");
+			console.log("Text on the element is " + textOfElement);
+			
+			/*
+				Note: If you pay attention to the output of this test you will see that it contains
+				this find stratedgy {"using":"xpath","value":"//a[normalize-space() = \"Find me I have nothing in me!!\"]"}
+				which means that finding element by some text is actually converted to findByXpath and normalize-space() function
+				is used.				
+			*/
+			
+		});	
+	});
 
 
 
